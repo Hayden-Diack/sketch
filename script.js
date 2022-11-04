ebd064340ba479dbec55a27b1900ae68d573989c
SkysAreRed <skysarered2@gmail.com>
2022-11-04 16:52:18 +0100

    create mode to target bground color

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const DEFAULT_SIZE = 16;
 const DEFAULT_MODE = 'color';
 
 let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
 
 function setCurrentColor(newColor) {
     currentColor = newColor
@@ -16,25 +18,30 @@ document.body.onmousedown = () => (mouseDown = true); // can draw
 document.body.onmouseup = () => (mouseDown = false); // cant draw
 
 function createGrid(gridSize) {
-    //grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; 
-    //grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; 
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     for (let i = 0; i < (gridSize * gridSize); i++) {
         const gridElement = document.createElement('div');
-        grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; 
-        grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover' , changeColor)
+        gridElement.addEventListener('mousedown' , changeColor)
         grid.appendChild(gridElement);
     }
 }
 
-function colorChoice() {
-    const grids = grid.querySelectorAll('.grid-element');
-    
-    colorBtn.textContent = 'Colour Button'; // applies colour button name text via DOM
+function colorChoice(e) { // e is the let value that holds the colour
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    }
 
-    colorBtn.addEventListener = ('click' , () => {
-        let Rnum
-    })
+    //const grids = grid.querySelectorAll('.grid-element');
+    
+    //colorBtn.textContent = 'Colour Button'; // applies colour button name text via DOM
+
+    //colorBtn.addEventListener = ('click' , () => {
+    //    let Rnum
+    //})
 
 }
 
